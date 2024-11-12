c05ced6e7db0a5ea9413cd3786ee68b2e4cb9aea
Christopher Morse <cmor0708@gmail.com>
2024-11-11 20:26:08 -0800

    Add files via upload

---
+++ Morse_CourseProject_Part1/MainForm.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Morse_CourseProject_Part1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.titleText = new System.Windows.Forms.TextBox();
+            this.urlText = new System.Windows.Forms.TextBox();
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.yearText = new System.Windows.Forms.TextBox();
+            this.yearLabel = new System.Windows.Forms.Label();
+            this.genreText = new System.Windows.Forms.TextBox();
+            this.genreLabel = new System.Windows.Forms.Label();
+            this.artistText = new System.Windows.Forms.TextBox();
+            this.artistLabel = new System.Windows.Forms.Label();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.outputText = new System.Windows.Forms.TextBox();
+            this.songList = new System.Windows.Forms.ListBox();
+            this.allSongsButton = new System.Windows.Forms.Button();
+            this.songListLabel = new System.Windows.Forms.Label();
+            this.detailsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(45, 37);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(58, 26);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Title:";
+            // 
+            // titleText
+            // 
+            this.titleText.Location = new System.Drawing.Point(134, 34);
+            this.titleText.Name = "titleText";
+            this.titleText.Size = new System.Drawing.Size(316, 32);
+            this.titleText.TabIndex = 0;
+            this.titleText.TextChanged += new System.EventHandler(this.titleText_TextChanged);
+            // 
+            // urlText
+            // 
+            this.urlText.Location = new System.Drawing.Point(134, 259);
+            this.urlText.Name = "urlText";
+            this.urlText.Size = new System.Drawing.Size(316, 32);
+            this.urlText.TabIndex = 4;
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(45, 265);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(62, 26);
+            this.urlLabel.TabIndex = 2;
+            this.urlLabel.Text = "URL:";
+            // 
+            // yearText
+            // 
+            this.yearText.Location = new System.Drawing.Point(134, 198);
+            this.yearText.Name = "yearText";
+            this.yearText.Size = new System.Drawing.Size(316, 32);
+            this.yearText.TabIndex = 3;
+            // 
+            // yearLabel
+            // 
+            this.yearLabel.AutoSize = true;
+            this.yearLabel.Location = new System.Drawing.Point(45, 201);
+            this.yearLabel.Name = "yearLabel";
+            this.yearLabel.Size = new System.Drawing.Size(65, 26);
+            this.yearLabel.TabIndex = 4;
+            this.yearLabel.Text = "Year:";
+            // 
+            // genreText
+            // 
+            this.genreText.Location = new System.Drawing.Point(134, 141);
+            this.genreText.Name = "genreText";
+            this.genreText.Size = new System.Drawing.Size(316, 32);
+            this.genreText.TabIndex = 2;
+            // 
+            // genreLabel
+            // 
+            this.genreLabel.AutoSize = true;
+            this.genreLabel.Location = new System.Drawing.Point(45, 144);
+            this.genreLabel.Name = "genreLabel";
+            this.genreLabel.Size = new System.Drawing.Size(78, 26);
+            this.genreLabel.TabIndex = 6;
+            this.genreLabel.Text = "Genre:";
+            // 
+            // artistText
+            // 
+            this.artistText.Location = new System.Drawing.Point(134, 83);
+            this.artistText.Name = "artistText";
+            this.artistText.Size = new System.Drawing.Size(316, 32);
+            this.artistText.TabIndex = 1;
+            // 
+            // artistLabel
+            // 
+            this.artistLabel.AutoSize = true;
+            this.artistLabel.Location = new System.Drawing.Point(45, 89);
+            this.artistLabel.Name = "artistLabel";
+            this.artistLabel.Size = new System.Drawing.Size(68, 26);
+            this.artistLabel.TabIndex = 8;
+            this.artistLabel.Text = "Artist:";
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(134, 331);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(227, 88);
+            this.AddButton.TabIndex = 5;
+            this.AddButton.Text = "Add Song";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // outputText
+            // 
+            this.outputText.Location = new System.Drawing.Point(481, 360);
+            this.outputText.Multiline = true;
+            this.outputText.Name = "outputText";
+            this.outputText.Size = new System.Drawing.Size(372, 272);
+            this.outputText.TabIndex = 11;
+            this.outputText.TabStop = false;
+            this.outputText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // songList
+            // 
+            this.songList.FormattingEnabled = true;
+            this.songList.HorizontalScrollbar = true;
+            this.songList.ItemHeight = 25;
+            this.songList.Location = new System.Drawing.Point(481, 42);
+            this.songList.Name = "songList";
+            this.songList.ScrollAlwaysVisible = true;
+            this.songList.Size = new System.Drawing.Size(372, 229);
+            this.songList.TabIndex = 12;
+            this.songList.TabStop = false;
+            // 
+            // allSongsButton
+            // 
+            this.allSongsButton.Location = new System.Drawing.Point(134, 445);
+            this.allSongsButton.Name = "allSongsButton";
+            this.allSongsButton.Size = new System.Drawing.Size(227, 88);
+            this.allSongsButton.TabIndex = 6;
+            this.allSongsButton.Text = "Show All Songs";
+            this.allSongsButton.UseVisualStyleBackColor = true;
+            this.allSongsButton.Click += new System.EventHandler(this.allSongsButton_Click);
+            // 
+            // songListLabel
+            // 
+            this.songListLabel.AutoSize = true;
+            this.songListLabel.Location = new System.Drawing.Point(481, 13);
+            this.songListLabel.Name = "songListLabel";
+            this.songListLabel.Size = new System.Drawing.Size(109, 26);
+            this.songListLabel.TabIndex = 14;
+            this.songListLabel.Text = "Song List:";
+            // 
+            // detailsLabel
+            // 
+            this.detailsLabel.AutoSize = true;
+            this.detailsLabel.Location = new System.Drawing.Point(481, 331);
+            this.detailsLabel.Name = "detailsLabel";
+            this.detailsLabel.Size = new System.Drawing.Size(85, 26);
+            this.detailsLabel.TabIndex = 15;
+            this.detailsLabel.Text = "Details:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 644);
+            this.Controls.Add(this.detailsLabel);
+            this.Controls.Add(this.songListLabel);
+            this.Controls.Add(this.allSongsButton);
+            this.Controls.Add(this.songList);
+            this.Controls.Add(this.outputText);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.artistText);
+            this.Controls.Add(this.artistLabel);
+            this.Controls.Add(this.genreText);
+            this.Controls.Add(this.genreLabel);
+            this.Controls.Add(this.yearText);
+            this.Controls.Add(this.yearLabel);
+            this.Controls.Add(this.urlText);
+            this.Controls.Add(this.urlLabel);
+            this.Controls.Add(this.titleText);
+            this.Controls.Add(this.titleLabel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "MainForm";
+            this.Text = "Video Manager";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox titleText;
+        private System.Windows.Forms.TextBox urlText;
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.TextBox yearText;
+        private System.Windows.Forms.Label yearLabel;
+        private System.Windows.Forms.TextBox genreText;
+        private System.Windows.Forms.Label genreLabel;
+        private System.Windows.Forms.TextBox artistText;
+        private System.Windows.Forms.Label artistLabel;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox outputText;
+        private System.Windows.Forms.ListBox songList;
+        private System.Windows.Forms.Button allSongsButton;
+        private System.Windows.Forms.Label songListLabel;
+        private System.Windows.Forms.Label detailsLabel;
+    }
+}
+
---
+++ Morse_CourseProject_Part1/MainForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Morse_CourseProject_Part1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder(outputText.Text);
+            string nl = "\r\n";
+
+           if(string.IsNullOrEmpty(titleText.Text))
+            {
+                // Title is blank
+                MessageBox.Show("The song title cannot be blank.");
+            }
+           else if(string.IsNullOrEmpty(artistText.Text))
+            {
+                // Artist is blank
+                MessageBox.Show("The artist cannot be blank.");
+            }
+            else if (string.IsNullOrEmpty(genreText.Text))
+            {
+                // Genre is blank
+                MessageBox.Show("The genre cannot be blank.");
+            }
+            else if (string.IsNullOrEmpty(yearText.Text))
+            {
+                // Year is blank
+                MessageBox.Show("The year cannot be blank.");
+            }
+            else if (string.IsNullOrEmpty(urlText.Text))
+            {
+                // URL is blank
+                MessageBox.Show("The url cannot be blank.");
+            }
+            else
+            {
+                // Add title to ListBox
+                songList.Items.Add(titleText.Text);
+                // Build the output text
+                 sb.Append(titleText.Text);
+                 sb.Append(nl);
+                 sb.Append(artistText.Text);
+                sb.Append(nl);
+                sb.Append(genreText.Text);
+                sb.Append(nl);
+                sb.Append(yearText.Text);
+                sb.Append(nl);
+                sb.Append(urlText.Text);
+                sb.Append(nl);
+                sb.Append(nl);   
+
+            outputText.Text = sb.ToString();
+            }
+
+           
+        }
+
+        private void titleText_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void allSongsButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder(string.Empty);
+            string nl = "\r\n";
+
+            // Build the output text
+            foreach (var item in songList.Items)
+            {
+                sb.Append(item.ToString());
+                sb.Append(nl);
+            }
+         //Put the output text into the output textbox
+         outputText.Text = sb.ToString();
+        }
+
+
+    }
+}
